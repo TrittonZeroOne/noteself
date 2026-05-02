3ee6aa6fe29834cd1eb7a1780681a29cc515441e
TrittonZeroOne <56587584+TrittonZeroOne@users.noreply.github.com>
2026-05-02 09:34:59 +0700

    first commit

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -47,4 +47,5 @@ $routes->group('', ['filter' => 'auth'], function($routes) {
     $routes->get('/plans/export', 'PlanController::export');
     $routes->get('/plans/complete/(:num)', 'PlanController::complete/$1');
     $routes->get('/plans/uncomplete/(:num)', 'PlanController::uncomplete/$1');
+    $routes->get('/plans/view/(:num)', 'PlanController::view/$1'); // <-- TAMBAH INI
 });
